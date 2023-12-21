e72a53bdc40839842564a36189bcfe280da2be78
LYH <peperony367@gmail.com>
2023-12-21 11:05:20 +0900

    파이어베이스 키  env 파일에 넣음

--- src/fbase.js
+++ src/fbase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDKASbf_2s_H_vmh0UfaTcoVZ2zd158zw8",
-    authDomain: "team1-4b3ca.firebaseapp.com",
-    projectId: "team1-4b3ca",
-    storageBucket: "team1-4b3ca.appspot.com",
-    messagingSenderId: "891722785943",
-    appId: "1:891722785943:web:c12b3a38e8d997cca8f5d2"
+    apiKey: process.env.REACT_APP_API,
+    authDomain: process.env.REACT_APP_AUTH,
+    projectId: process.env.REACT_APP_P,
+    storageBucket: process.env.REACT_APP_S,
+    messagingSenderId: process.env.REACT_APP_M,
+    appId: process.env.REACT_APP_APPID
   };
 
 const app = initializeApp(firebaseConfig);
