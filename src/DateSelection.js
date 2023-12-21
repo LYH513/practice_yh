39804be32b75e36f131c78636525afcc1a1fef48
LYH <peperony367@gmail.com>
2023-12-21 23:35:16 +0900

    날짜 화면 표시 수정함

--- src/DateSelection.js
+++ src/DateSelection.js
@@ -4,71 +4,64 @@ import Datetime from 'react-datetime';
 import 'moment/locale/ko'; // 한국어 로케일 추가
 import moment from 'moment';
 
+// 수정해야함
+
 function DateSelection() {
-    const [date, setDate] = useState('');
-    const [open, setOpen] = useState(false);
-    const format = 'YYYY-MM-DD';
+  const [date, setDate] = useState('');
+  const [open, setOpen] = useState(false);
+  const format = 'YYYY-MM-DD';
 
-    const handleClickButton = () => {
-        setOpen(!open);
-    };
+  //const [diffDays, setDiffDays] = useState(null);
+  const [current, setCurrent] = useState(moment()); // 현재 날짜를 상태로 관리
 
-    const handleChangeCalendar = (selected) => {
-        const formattedDate = selected.format(format);
-        setDate(formattedDate);
-        setOpen(false);
-    };
-  const getSeparator = () => {
-    const regex = /[^0-9a-zA-Z]+/;
-    const match = format.match(regex);
-
-    if (match) {
-      const symbol = match[0];
-      const indexes = [];
-
-      for (let i = 0; i < format.length; i++) {
-        if (format[i] === symbol) {
-          indexes.push(i);
-        }
-      }
-
-      return { symbol, indexes };
-    }
-    return { symbol: undefined, indexes: [] };
+  const handleClickButton = () => {
+    setOpen(!open);
   };
 
-  const separator = getSeparator();
+  const handleChangeCalendar = (selected) => {
+    const formattedDate = selected.format(format);
+    
+    // 시간 부분을 무시하고 날짜 부분만 고려한 차이를 계산
+    //const daysDifference = selected.startOf('day').diff(current.startOf('day'), 'days');
+    //setDiffDays(daysDifference);
   
+    // 현재 날짜를 업데이트
+    setCurrent(current);
+  
+    setDate(formattedDate);
+    setOpen(false);
+  };
+
   useEffect(() => {
-    console.log(separator);
-    // { symbol: '-', indexes: [4, 7] }
-  }, []);
+    // 현재 날짜가 변경될 때마다 로그 출력
+    console.log('현재 날짜가 업데이트되었음:', current);
+    
+  }, [current]); // current 값이 변경될 때만 useEffect 실행
 
   const handleChangeDate = (e) => {
-    let currentDate = e.target.value;
-
-    setDate(currentDate);
+    let selectDate = e.target.value;
+    setDate(selectDate);
   };
 
-// 현재 날짜 이후의 날짜만 선택 가능하도록 설정
-  const isValidDate = (current) => {
-    return current.isSameOrAfter(moment(), 'day');
+  const isValidDate = (select) => {
+    return select.isSameOrAfter(current, 'day');
   };
 
   return (
     <div>
-      <input
-        type='text'
-        value={date}
-        placeholder='날짜 선택'
-        onChange={handleChangeDate}
-        disabled
-      />
+      <div>
+        <input
+          type='text'
+          value={date}
+          placeholder='날짜 선택'
+          onChange={handleChangeDate}
+          disabled
+        />
 
-      <button type='button' onClick={handleClickButton}>
-        <BsFillCalendarHeartFill />
-      </button>
-      {open && (
+        <button type='button' onClick={handleClickButton}>
+          <BsFillCalendarHeartFill />
+        </button>
+        {open && (
           <Datetime
             input={false}
             timeFormat={false}
@@ -78,10 +71,14 @@ function DateSelection() {
             locale="ko"
             isValidDate={isValidDate}
           />
-      )}
-      
+        )}
+      </div>
+      <div>
+        <p>지금 날짜 표시: {current.format(format)}</p>
+        <p>선택한 날짜 표시: {date}</p>
+      </div>
     </div>
   );
-};
+}
 
 export default DateSelection;
